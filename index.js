5156d678d7aa46e952361bc277e712e980e1f2da
Kioshi Okamoto <kjor29@gmail.com>
2023-06-28 01:56:06 -0500

    update intersection observer

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-const elements = document.querySelectorAll('.navbar--element');
+const navbar = document.querySelectorAll('.navbar--element');
+const elements = document.querySelectorAll('.page-view');
+
+var options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.5
+};
+
+function callback(entries, observer) {
+    entries.forEach(function (entry) {
+        if (entry.isIntersecting) {
+            navbar.forEach((element) => element.classList.remove('navbar--item-active'))
+            if (entry.target.id === "hero") return
+
+            const element = document.querySelectorAll(`[href*="${entry.target.id}"]`)?.[0];
+            element.classList.add('navbar--item-active')
+        }
+    });
+}
+
+var observer = new IntersectionObserver(callback, options);
 
 elements.forEach(function (element) {
-    element.addEventListener('click', function () {
-        elements.forEach(function (el) {
-            el.classList.remove('navbar--item-active');
-        });
-
-        element.classList.add('navbar--item-active');
-    });
+    observer.observe(element);
 });
